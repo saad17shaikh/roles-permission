3fadee9a95df0505f436f4cbdf5d7c93b0adb61e
saadshaikh17 <saad@gitlab.techlogixit.com>
2024-11-01 11:49:55 +0530

    feat: restructure folders for better organization and scalability with next js project initialization

---
+++ client/app/auth/login/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>User Login</div>;
+};
+
+export default page;
---
+++ client/app/superadmin/login/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Super Admin Login</div>;
+};
+
+export default page;
